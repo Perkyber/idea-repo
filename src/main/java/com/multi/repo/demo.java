a08f010013b99042d0826df82f1748216a4f1b75
MULTI-XP <xupeng332@outlook.com>
2019-11-06 11:55:07 +0800

    第一次提交

---
+++ src/main/java/com/multi/repo/demo.java
@@ -0,0 +1,13 @@
+package com.multi.repo;
+
+/**
+ * @author by multi.xp
+ * @version V1.0
+ * @date 2019/11/6 11:47
+ * @description:
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("ab");
+    }
+}
